caa9eb1fb5cf188fef28f4313a8fcae0c6b56f0d
Мун Александр Илларионович <a.mun@alataucitybank.kz>
2025-12-06 20:26:02 +0500

    [DEMO][D-0003] DirectExchange

---
+++ src/main/java/kz/mun/demo/config/MessageQueueConfig.java
@@ -0,0 +1,49 @@
+package kz.mun.demo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageQueueConfig {
+    public static final String SEND_MESSAGE_EXCHANGE = "send-message-x";
+    public static final String MESSAGE_EVENTS_ROUTING_KEY = "message-events";
+
+    public static final String SEND_MESSAGE_QUEUE = "send-message-q";
+    public static final String PROCESS_MESSAGE_QUEUE = "process-message-q";
+
+    @Bean
+    DirectExchange sendMessageExchange() {
+        return new DirectExchange(SEND_MESSAGE_EXCHANGE);
+    }
+
+    @Bean
+    Queue sendMessageQueue() {
+        return QueueBuilder
+                .durable(SEND_MESSAGE_QUEUE)
+                .build();
+    }
+
+    @Bean
+    Binding sendMessageBinding(Queue sendMessageQueue, DirectExchange sendMessageExchange) {
+        return BindingBuilder
+                .bind(sendMessageQueue)
+                .to(sendMessageExchange)
+                .with(MESSAGE_EVENTS_ROUTING_KEY);
+    }
+
+    @Bean
+    Queue processMessageQueue() {
+        return QueueBuilder
+                .durable(PROCESS_MESSAGE_QUEUE)
+                .build();
+    }
+
+    @Bean
+    Binding processMessageBinding(Queue processMessageQueue, DirectExchange sendMessageExchange) {
+        return BindingBuilder
+                .bind(processMessageQueue)
+                .to(sendMessageExchange)
+                .with(MESSAGE_EVENTS_ROUTING_KEY);
+    }
+}
--- src/main/java/kz/mun/demo/service/impl/PushMessageServiceImpl.java
+++ src/main/java/kz/mun/demo/service/impl/PushMessageServiceImpl.java
@@ -1,16 +1,36 @@
 package kz.mun.demo.service.impl;
 
 import kz.mun.demo.model.Contact;
+import kz.mun.demo.model.Message;
+import kz.mun.demo.model.MessageType;
 import kz.mun.demo.model.PushType;
 import kz.mun.demo.service.PushMessageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+import static kz.mun.demo.config.MessageQueueConfig.MESSAGE_EVENTS_ROUTING_KEY;
+import static kz.mun.demo.config.MessageQueueConfig.SEND_MESSAGE_EXCHANGE;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PushMessageServiceImpl implements PushMessageService {
+
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void send(Contact contact, PushType pushType) {
         log.info(contact.toString());
+        Message pushMessage = Message.builder()
+                .id(UUID.randomUUID())
+                .messageType(MessageType.SMS)
+                .messageBody("test body")
+                .contact(contact)
+                .build();
+        rabbitTemplate.convertAndSend(SEND_MESSAGE_EXCHANGE, MESSAGE_EVENTS_ROUTING_KEY, pushMessage);
     }
 }
